80c31628eddf9c03a83ad7a8266fbba4d1dd1589
brherr <herrbrad@yahoo.com>
2023-04-29 12:35:45 -0400

    commit

--- src/MuiGrid.jsx
+++ src/MuiGrid.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
@@ -14,45 +13,44 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function BasicGrid() {
   return (
-    <Box sx={{ flexGrow: 1, }}>
       <Grid container spacing={2}>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={2}>
           <Item>Item 1</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={4} >
           <Item>Item 2</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={6}>
           <Item>Item 3</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={2}>
           <Item>Item 4</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={4}>
           <Item>Item 5</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={6}>
           <Item>Item 6</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={2}>
           <Item>Item 7</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={4}>
           <Item>Item 8</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={6}>
           <Item>Item 9</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={2}>
           <Item>Item 10</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={4}>
           <Item>Item 11</Item>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} md={6}>
           <Item>Item 12</Item>
         </Grid>
       </Grid>
-    </Box>
+
   );
 }
